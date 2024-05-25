07740792e38773e7600b8843d2e89d5777c1f799
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-25 13:52:58 -0500

    P1 project - compressor

--- compressor1/src/main/java/life/growthhungry/academy/p1/CompressionInfoHolder.java
+++ compressor1/src/main/java/life/growthhungry/academy/p1/CompressionInfoHolder.java
@@ -3,5 +3,20 @@ package life.growthhungry.academy.p1;
 import java.util.Map;
 
 public class CompressionInfoHolder {
-    Map<Short, String> codeMap;
+    private final Map<Short, String> codeToWord;
+    private final byte[] codedText;
+
+    public CompressionInfoHolder(Map<Short, String> codeToWord, byte[] codedText) {
+
+        this.codeToWord = codeToWord;
+        this.codedText = codedText;
+    }
+
+    public Map<Short, String> getCodeToWord() {
+        return codeToWord;
+    }
+
+    public byte[] getCodedText() {
+        return codedText;
+    }
 }
--- compressor1/src/main/java/life/growthhungry/academy/p1/Main.java
+++ compressor1/src/main/java/life/growthhungry/academy/p1/Main.java
@@ -10,12 +10,13 @@ public class Main {
         String outputFile = inputFile + ".sc";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+
+            ByteArrayOutputStream codedText = new ByteArrayOutputStream();
 
             // using Short as it's enough for our program
             Map<String, Short> wordToCode= new HashMap<>();
             Map<Short, String> codeToWord= new HashMap<>();
-            short codeCounter = 0;
+            short codeCounter = 0; // or Short.MIN_VALUE
             String line = reader.readLine();
             while (line != null) {
               String[] words = line.split("(?<=\\s)|(?=\\s)");
@@ -25,6 +26,18 @@ public class Main {
                  if (existingCode == null) {
                      wordToCode.put(w, codeCounter);
                      codeToWord.put(codeCounter, w);
+
+
+                     // explanation: short has 2 bites : 01011001(high) 01111100(low)
+                     // divide short into 2 bytes; we shift short to 8 bites
+                     // the low bite is now lost, and high is now low
+                     byte high = (byte) (codeCounter >>> 8); // this operation does >> 00000000 01011001 => 01011001
+                     // everything bigger than 1 bite is cut off
+                     byte low = (byte) codeCounter; // this operation does >> 01111100
+                     codedText.write(high);
+                     codedText.write(low);
+
+
                      codeCounter++;
                      if (codeCounter == Short.MAX_VALUE) {
                          throw new CompressionException("Too many words in this file");
@@ -35,6 +48,10 @@ public class Main {
               System.out.println();
               line = reader.readLine();
             }
+
+            ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+            CompressionInfoHolder holder = new CompressionInfoHolder(codeToWord, codedText.toByteArray());
+
             System.out.println("Number of words " + codeCounter);
 
 
